6e17cff3f5bcf8e952e2b805cb9cc15cb3298451
Leon Kaspras <leon@Air-von-Leon.fritz.box>
2022-09-12 23:08:27 +0200

    der letzte commit

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ soup = BeautifulSoup(page.text, "html.parser")
 table = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
 # print(table)
 
-data = table.findAll('tr')
-print(data)
+data = table.findAll('td')
+
+# Ganzen Attribute sind in <b>´s -> diese durchsuchen mit immer next und dann extern einfach den Tag nach diesem
+# als value einfügen
+
+[print(type(element.b)) for element in data]
+
+#last_a_tag = soup.find("b")
+#print(last_a_tag.next_element)
+
+#data_key = table.findAll('b')
+#print(data_key)
+
 
