71380f47c563ed56396d5f2f44b89efbb518cb80
Leon Kaspras <leon@MacBook-Air-von-Leon.fritz.box>
2022-09-15 15:15:41 +0200

    Added a loop to print out every element that I require for the task as a string

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ page: requests.Response = requests.get(cr7_url)
 soup: bs4.BeautifulSoup = BeautifulSoup(page.text, "html.parser")
 soup.prettify()
 
+
 # die Tabelle mit den gewünschten Daten
 table: bs4.element.Tag = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
-# print(table)
 
 data: bs4.element.ResultSet = table.findAll('td')
 
-# Ganzen Attribute sind in <b>´s -> diese durchsuchen mit immer next und dann extern einfach den Tag nach diesem
-# als value einfügen
+# gibt alle Elemete von data aus
+for data in data:
+    print(type(data.text))
 
-# [print(type(element.b)) for element in data]
 
-#last_a_tag = soup.find("b")
-#print(last_a_tag.next_element)
 
-#data_key = table.findAll('b')
-#print(data_key)
+
+
+
+
+
+
+
+
 
 
