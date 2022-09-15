3146bbdd7c91ddb63732691d5cd1c56a26ea89c8
Leon Kaspras <leon@MacBook-Air-von-Leon.fritz.box>
2022-09-15 14:38:46 +0200

    added type hints for better readability of the code

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from bs4 import BeautifulSoup
+import bs4
 import requests
 
-cr7_url = "https://de.wikipedia.org/wiki/Cristiano_Ronaldo"
-page = requests.get(cr7_url)
+cr7_url: str = "https://de.wikipedia.org/wiki/Cristiano_Ronaldo"
+page: requests.Response = requests.get(cr7_url)
 
-soup = BeautifulSoup(page.text, "html.parser")
-# print(soup.prettify())
+soup: bs4.BeautifulSoup = BeautifulSoup(page.text, "html.parser")
+soup.prettify()
 
 # die Tabelle mit den gewünschten Daten
-table = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
+table: bs4.element.Tag = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
 # print(table)
 
-data = table.findAll('td')
+data: bs4.element.ResultSet = table.findAll('td')
 
 # Ganzen Attribute sind in <b>´s -> diese durchsuchen mit immer next und dann extern einfach den Tag nach diesem
 # als value einfügen
 
-[print(type(element.b)) for element in data]
+# [print(type(element.b)) for element in data]
 
 #last_a_tag = soup.find("b")
 #print(last_a_tag.next_element)
