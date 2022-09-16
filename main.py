c8ec294c78837d57f273be80e29c226ef48fd4eb
Leon Kaspras <leon@MacBook-Air-von-Leon.fritz.box>
2022-09-16 16:43:37 +0200

    Added a loop to iterate over the data and found a way to find specific elements including the next one that I want including adding those to a dictionary.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import bs4
 import requests
 
+
 cr7_url: str = "https://de.wikipedia.org/wiki/Cristiano_Ronaldo"
 page: requests.Response = requests.get(cr7_url)
 
@@ -12,11 +13,20 @@ soup.prettify()
 # die Tabelle mit den gewünschten Daten
 table: bs4.element.Tag = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
 
-data: bs4.element.ResultSet = table.findAll('td')
+data_td_tags: bs4.element.ResultSet = table.findAll('td')
 
-# gibt alle Elemete von data aus
-for data in data:
-    print(type(data.text))
+# dictionary, um die Daten zur Person zu speichern
+person: dict = {}
+
+
+for data in data_td_tags:
+    if data.text.find("Geburtstag") == 0:
+        # sucht das gewünschte Element heraus
+        element: bs4.element.Tag = soup.find(text="Geburtstag").findNext('td').contents[0]
+        #print(a.next_element)
+        person.update({"Geburtstag":element.next_element})
+
+print(person)
 
 
 
