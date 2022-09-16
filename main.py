13fb7aa5681579168fd94e3b2bc9e0e3cc344181
Leon Kaspras <leon@MacBook-Air-von-Leon.fritz.box>
2022-09-16 17:06:58 +0200

    Completed the dictionary for the personal data of Cristiano Ronaldo

--- main.py
+++ main.py
@@ -20,14 +20,36 @@ person: dict = {}
 
 
 for data in data_td_tags:
+    if data.text.find("Voller Name") == 0:
+        # sucht das gewünschte Element heraus
+        element: bs4.element.Tag = soup.find(text="Voller Name").findNext('td').contents[0]
+        person.update({"Voller Name":element})
+
     if data.text.find("Geburtstag") == 0:
         # sucht das gewünschte Element heraus
         element: bs4.element.Tag = soup.find(text="Geburtstag").findNext('td').contents[0]
         #print(a.next_element)
         person.update({"Geburtstag":element.next_element})
 
-print(person)
+    if data.text.find("Geburtsort") == 0:
+        # sucht das gewünschte Element heraus
+        element: bs4.element.Tag = soup.find(text="Geburtsort").findNext('td').contents[0]
+        person.update({"Geburtsort":element.next_element+", "+ element.findNext('a').text})
 
+    if data.text.find("Größe") == 0:
+        # sucht das gewünschte Element heraus
+        element: bs4.element.Tag = soup.find(text="Größe").findNext('td').contents[0]
+        #print(element)
+        person.update({"Größe":element})
+
+    if data.text.find("Position") == 0:
+        # sucht das gewünschte Element heraus
+        element: bs4.element.Tag = soup.find(text="Position").findNext('td').contents[0]
+        person.update({"Position":element.next_element+", "+ element.findNext('a').text})
+
+
+
+print(person)
 
 
 
