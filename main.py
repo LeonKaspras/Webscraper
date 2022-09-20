684d5e6ea3b245f6bf1cab3deb447ec0ff348b8c
Leon Kaspras <leon@MacBook-Air-von-Leon.fritz.box>
2022-09-20 23:52:00 +0200

    Changed the type of birthday to datetime object and added the Age out of the birthday to the dictionary

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from bs4 import BeautifulSoup
 import bs4
 import requests
-from datetime import datetime
+from datetime import datetime, date
+import locale
 
-print("Please enter the url of a footballplayer: ")
-url:str = input()
-#url: str = "https://de.wikipedia.org/wiki/Cristiano_Ronaldo"
+locale.setlocale(locale.LC_TIME, "de_DE") # german
+
+
+
+#print("Please enter the url of a footballplayer: ")
+#url:str = input()
+
+url: str = "https://de.wikipedia.org/wiki/Cristiano_Ronaldo"
 page: requests.Response = requests.get(url)
 
 soup: bs4.BeautifulSoup = BeautifulSoup(page.text, "html.parser")
 soup.prettify()
-
-
 
 # die Tabelle mit den gewünschten Daten
 table: bs4.element.Tag = soup.find('table',{'class':'infobox toccolours float-right toptextcells'})
@@ -20,7 +24,6 @@ data_td_tags: bs4.element.ResultSet = table.findAll('td')
 
 # dictionary, um die Daten zur Person zu speichern
 person: dict = {}
-
 
 for data in data_td_tags:
     if data.text.find("Voller Name") == 0:
@@ -33,7 +36,13 @@ for data in data_td_tags:
     if data.text.find("Geburtstag") == 0:
         # sucht das gewünschte Element heraus
         element: bs4.element.Tag = soup.find(text="Geburtstag").findNext('td').contents[0]
-        person.update({"Geburtstag":element.next_element+" "+element.findNext('a').text})
+        bday = element.next_element+" "+element.findNext('a').text
+        datetime_format: str = "%d. %B %Y"
+        bday: datetime = datetime.strptime(bday, datetime_format)
+        today = date.today()
+        age: int = today.year - bday.year - ((today.month, today.day) < (bday.month, bday.day))
+        person.update({"Geburtstag":bday})
+        person.update({"Alter":age})
 
     if data.text.find("Geburtsort") == 0:
         # sucht das gewünschte Element heraus
